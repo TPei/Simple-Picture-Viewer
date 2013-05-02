77bdbb91404fc44b7c4f0b03140334489490c91b
dragon5689 <dragon5689@gmx.net>
2013-05-02 17:23:09 +0200

    Added JButtons to MyFrame so that they can access ImageHolder object

--- MyFrame.java
+++ MyFrame.java
@@ -21,8 +21,72 @@ public class MyFrame extends JFrame
 		
 		ImageHolder image = new ImageHolder();
 		
-		//JPanel buttonContainer = createNavigation();
-		Navigation buttonContainer = new Navigation();
+		JPanel buttonContainer = new JPanel();
+		//Navigation buttonContainer = new Navigation();
+		
+		JButton nextButton = new JButton();
+        ImageIcon nextButtonImage = new ImageIcon("/Users/Thomas/Dropbox/Programming/OOP/Labore/src/labor5/icons/right.png");
+        nextButton.setIcon(nextButtonImage);
+        
+        JButton prevButton = new JButton();
+        ImageIcon prevButtonImage = new ImageIcon("/Users/Thomas/Dropbox/Programming/OOP/Labore/src/labor5/icons/left.png");
+        prevButton.setIcon(prevButtonImage);
+        
+        // zoom buttons
+        JButton zoomOut = new JButton();
+        ImageIcon zoomOutImage = new ImageIcon("/Users/Thomas/Dropbox/Programming/OOP/Labore/src/labor5/icons/zoomOut.png");
+        zoomOut.setIcon(zoomOutImage);
+        
+        JButton zoomReal = new JButton();
+        ImageIcon zoomRealImage = new ImageIcon("/Users/Thomas/Dropbox/Programming/OOP/Labore/src/labor5/icons/default.png");
+        zoomReal.setIcon(zoomRealImage);
+        
+        JButton zoomIn = new JButton();
+        ImageIcon zoomInImage = new ImageIcon("/Users/Thomas/Dropbox/Programming/OOP/Labore/src/labor5/icons/zoomIn.png");
+        zoomIn.setIcon(zoomInImage);
+        
+        
+        // action listeners
+        nextButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		System.out.println("next Picture");
+        		int thisPicture = image.getWhichPicture();
+        		image.setWhichPicture(thisPicture++);
+        	}
+		});
+        
+        prevButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		System.out.println("previous Picture");
+        	}
+		});
+        
+        zoomOut.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		System.out.println("zoom out");
+        	}
+		});
+        
+        zoomReal.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		System.out.println("default zoom");
+        	}
+		});
+        
+        zoomIn.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		System.out.println("zoom in");
+        	}
+		});
+        
+        
+        
+        // add zoom and navigation buttons to JPanel
+        buttonContainer.add(zoomOut);
+        buttonContainer.add(zoomReal);
+        buttonContainer.add(zoomIn);
+        buttonContainer.add(prevButton);
+        buttonContainer.add(nextButton);
         
         // add image to center of frame
         add(image, BorderLayout.CENTER);
